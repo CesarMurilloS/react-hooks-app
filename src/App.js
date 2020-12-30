58129f6a9e9a4676344dc9bca8a656aac73d2dd9
Cesar Murillo <murilloscesar@gmail.com>
2020-12-29 20:02:07 -0600

    04 react hooks, hook form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import Contador from './components/Contador'
-import Formulario from './components/Formulario';
-import Lista from './components/Lista';
-import JsxAccion from './components/JsxAccion';
+import HookForm from './components/HookForm';
 
 function App() {
   return (
     <div className="container mt-5">
-    <Formulario></Formulario>
+    <HookForm></HookForm>
     </div>
   );
 }
